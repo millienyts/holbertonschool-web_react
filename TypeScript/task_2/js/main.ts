f40e56e7bcf366c51cfd3275ff4ebd866cbbede3
Millienyts <tmillieny@gmail.com>
2024-12-08 22:10:51 -0400

    task 5

---
+++ TypeScript/task_2/js/main.ts
@@ -0,0 +1,72 @@
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return "Working from home";
+  };
+  getCoffeeBreak(): string {
+    return "Getting a coffee break"
+  };
+  workDirectorTasks(): string {
+    return "Getting to director task";
+  }
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return "Cannot work from home";
+  }
+  getCoffeeBreak(): string {
+    return "Cannot have a break";
+  }
+  workTeacherTasks(): string {
+    return "Getting to work";
+  }
+}
+
+function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === 'number' && salary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
+}
+
+function isDirector(employee: Director | Teacher): employee is Director{
+  return employee instanceof Director;
+}
+
+function executeWork(employee: Director | Teacher): void {
+  if (isDirector(employee)) {
+    console.log(employee.workDirectorTasks());
+  } else {
+    console.log(employee.workTeacherTasks());
+  }
+}
+
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+  return `Teaching ${todayClass}`
+}
+
+console.log(createEmployee(200));
+Teacher
+console.log(createEmployee(1000));
+Director
+console.log(createEmployee('$500'));
+Director
+executeWork(createEmployee(200));
+executeWork(createEmployee(1000));
+teachClass('Math');
+teachClass('History');
