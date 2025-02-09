3960df3e35ce92d3aee71b96af83de9ff7ba2d9b
Millienyts <tmillieny@gmail.com>
2025-02-09 03:49:29 -0400

    react

---
+++ react_redux-part1/task_8/dashboard/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import BodySection from './components/BodySection/BodySection';
+
+const App = () => (
+  <div>
+    <Header />
+    <BodySection />
+    <Footer />
+  </div>
+);
+
+export default App;
