030df36f4e243fd79670407160213a8c35c62bb9
Millienyts <tmillieny@gmail.com>
2025-05-16 01:03:04 -0400

    react

--- react_props/task_0/dashboard/src/App/App.jsx
+++ react_props/task_0/dashboard/src/App/App.jsx
@@ -3,12 +3,10 @@ import './App.css';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Login from '../Login/Login';
-import Notifications from '../Notifications/Notifications'; // Asegúrate de tener este componente en task_2
 
 function App() {
   return (
     <Fragment>
-      <Notifications />
       <Header />
       <Login />
       <Footer />
