7aa593d2d0e4a22cd613913f42d94694df0d3c48
Millienyts <tmillieny@gmail.com>
2025-05-16 23:49:57 -0400

    Clean up .gitignore formatting and entries

---
+++ Webpack/task_3/modules/header/header.js
@@ -0,0 +1,10 @@
+import $ from 'jquery';
+import './header.css';
+import logo from '../../assets/holberton-logo.jpg';
+
+console.log('Init header');
+
+$('body').append(`
+  <div id="logo"></div>
+  <h1>Holberton Dashboard</h1>
+`);
---
+++ Webpack/task_3/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    header: './modules/header/header.js',
+    body: './modules/body/body.js',
+    footer: './modules/footer/footer.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  mode: 'development', // para dev server y debugging
+  devServer: {
+    static: './public',
+    port: 8564,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Holberton Dashboard',
+    }),
+  ],
+  devtool: 'inline-source-map',
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+};
